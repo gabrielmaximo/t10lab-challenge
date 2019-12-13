2f3ac14ee2f5b33298d2a70df19ca54564b516d2
gabrielmaximo <familia_maximo@hotmail.com>
2019-12-13 01:05:09 -0300

    init project

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,6 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Welcome to Omni CLI' }));
+routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
 
 export default routes;
--- src/server.js
+++ src/server.js
@@ -1,3 +1,10 @@
 import app from './app';
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log(
+    'App is running at http://%s:%d in %s mode',
+    process.env.APP_HOST,
+    process.env.APP_PORT,
+    process.env.NODE_ENV
+  );
+});
