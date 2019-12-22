366e4d6610fcfde6800aa5d68b19e718486cb6c7
gabrielmaximo <familia_maximo@hotmail.com>
2019-12-21 22:54:13 -0300

    deploy

--- src/server.js
+++ src/server.js
@@ -1,10 +1,3 @@
 import app from './app';
 
-app.listen(3333, () => {
-  console.log(
-    'App is running at http://%s:%d in %s mode',
-    process.env.APP_HOST,
-    process.env.APP_PORT,
-    process.env.NODE_ENV
-  );
-});
+app.listen(process.env.PORTs);
